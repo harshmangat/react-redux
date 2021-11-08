c5d89d7c70d5b314d0e7c706bb9b99b6d0324657
harshmangat <harshmangat0007@gmail.com>
2021-11-08 18:37:50 +0530

    added delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,13 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import Table from "./components/Table";
+
 
 import "./App.css";
 
-import {
-  INCREMENT,
-  DECREMENT,
-  RESET,
-  ADD_COUNT,
-} from "./redux/actions/counterActions";
-
 const App = () => {
-  const { count } = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-
-  return (
-    <div className="px-5 m-auto bg-green-100 border rounded-lg mt-44 w-max">
-      <p className="mt-1 text-2xl font-bold text-center text-green-600">
-        Counter App
-      </p>
-      <p className="mt-20 text-2xl font-bold text-center">{count}</p>
-
-      <div className="flex items-center justify-center gap-10 h-36">
-        <button
-          className=" btn btn-primary"
-          onClick={() => dispatch(INCREMENT())}
-        >
-          Increment
-        </button>
-
-        <button
-          className="rounded-full btn btn-secondary"
-          onClick={() => dispatch(DECREMENT())}
-        >
-          Decrement
-        </button>
-
-        <button
-          className=" btn btn-circle btn-lg"
-          onClick={() => dispatch(RESET())}
-        >
-          Reset
-        </button>
-
-        <button
-          className="btn btn-outline btn-accent"
-          onClick={() => dispatch(ADD_COUNT(10))}
-        >
-          Add on
-        </button>
-      </div>
-    </div>
-  );
+  return <>
+  <Table />
+  </>;
 };
 
 export default App;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,160 @@
-import React from 'react'
+import React from "react";
+import Header from "./header/Header";
 
 const Table = () => {
   return (
-    <div>
-      
+    <div className="">
+      <Header />
+      <div className="overflow-x-auto ">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-outline badge-sm">
+                  Desktop Support Technician
+                </span>
+              </td>
+              <td>Purple</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Brice Swyre</div>
+                    <div className="text-sm opacity-50">China</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Carroll Group
+                <br />
+                <span className="badge badge-outline badge-sm">
+                  Tax Accountant
+                </span>
+              </td>
+              <td>Red</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Marjy Ferencz</div>
+                    <div className="text-sm opacity-50">Russia</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Rowe-Schoen
+                <br />
+                <span className="badge badge-outline badge-sm">
+                  Office Assistant I
+                </span>
+              </td>
+              <td>Crimson</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Yancy Tear</div>
+                    <div className="text-sm opacity-50">Brazil</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Wyman-Ledner
+                <br />
+                <span className="badge badge-outline badge-sm">
+                  Community Outreach Specialist
+                </span>
+              </td>
+              <td>Indigo</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>
+                <button class="btn btn-circle btn-xs md:btn-sm lg:btn-md xl:btn-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    class="inline-block w-4 h-4 stroke-current md:w-6 md:h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    ></path>
+                  </svg>
+                </button>
+              </th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Table;
